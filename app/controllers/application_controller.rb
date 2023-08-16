27e0632e26e2ed2263e297ed343349c457e43e06
Khalid Abbas <khalidabbas.dev@gmail.com>
2023-08-16 16:56:39 -0700

    Enable flash alerts and notices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_exception
-    rescue_from ActionController::ParameterMissing, with: :rescue_exception
-    rescue_from ActiveRecord::NotNullViolation, with: :rescue_exception
-    rescue_from ArgumentError, with: :rescue_exception
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_exception_redirect_to_root
+  rescue_from ActionController::ParameterMissing, with: :rescue_exception
+  rescue_from ActiveRecord::NotNullViolation, with: :rescue_exception
+  rescue_from ArgumentError, with: :rescue_exception
+  rescue_from Faraday::ResourceNotFound, with: :rescue_exception
 
-    private
+  private
 
-    def rescue_exception(exception)
-        redirect_to trips_path, alert: exception.message, status: :not_found
-    end
+  def rescue_exception(exception)
+    flash[:alert] = exception.message
+    redirect_back(fallback_location: trips_path)
+  end
+
+  def rescue_exception_redirect_to_root(exception)
+    flash[:alert] = exception.message
+    redirect_back(fallback_location: root_path)
+  end
 end
